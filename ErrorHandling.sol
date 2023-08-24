faffbb4f0e438c15ac8642f0c5eeea4dd54a6438
Vishal Kumar <138686512+vishal206171@users.noreply.github.com>
2023-08-24 21:01:41 +0530

    Update ErrorHandling.sol

--- ErrorHandling.sol
+++ ErrorHandling.sol
@@ -1,29 +1,29 @@
-// SPDX-License-Identifier: MIT
+ // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
 contract Errorhandling {
-    bool public sunny = true;
-    bool public umbrella = false;
+    bool public sunday = true;
+    bool public books = false;
     uint finalCall = 0;
 
     
-    function weather () public {
-        require(sunny,"IT IS NOT SUNNY TODAY!!");
+    function yessunday () public {
+        require(sunday,"IT IS NOT SUNDAY TODAY!!");
         finalCall += 3;
     
         assert (finalCall != 0);
     }
-    function weatherChanger() public{
-        sunny = !sunny;
+    function dayChanger() public{
+        sunday = !sunday;
     }
     function getCal() public view returns(uint) {
         return finalCall;
     }
-    function BringUmbrella () public {
-        if(!sunny){
-            umbrella = true;
+    function BringBooks () public {
+        if(!sunday){
+            books = true;
         }else{
-            revert("No Umbrella needed");
+            revert("no need to come today");
         }
     }
 }
